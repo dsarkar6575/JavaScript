bbada47a980ac74e7fcb159c0543fcbd3dd0200b
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2023-11-08 12:27:43 +0000

    some

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("Dipankar Sarkar");
+// const accountId=365550;
+// let accountEmail="dipankarsarkar@gmail.com";
+// var accountPassword="1234";
+// // accountCity ="Bengalore";
+
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
