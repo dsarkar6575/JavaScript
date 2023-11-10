641d0df7d86d4741628bad274c62ee7471ed90d5
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2023-11-10 07:26:07 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=1234569;
+let accountEmail="dipankarsarkar@google.com";
+var accountPassword="12345";
+accountCity="Bengaluru";
+
+// accoutId=123;
+
+accountEmail="dsarkar@gmail.com";
+accountPassword="123";
+accountCity="Karnataka";
+
+/*
+prefer not to use var
+because of issues in block scope and function scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
