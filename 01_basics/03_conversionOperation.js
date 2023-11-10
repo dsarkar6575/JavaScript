77c6c977ed0be17db7c036e6268d2a184918e95e
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2023-11-10 10:57:47 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score="33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,false=>0
+//null=>0
+//undefined=>NaN
+
+let isLogin=2
+
+let booleanisLogin=Boolean(isLogin)
+console.log(booleanisLogin)
+
+
+//1=> true , 0=>false
+//""=>false
+//"Dipankar"=> true
+
+let someNumber=33
+
+let stringSomeNumber=String(someNumber)
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
